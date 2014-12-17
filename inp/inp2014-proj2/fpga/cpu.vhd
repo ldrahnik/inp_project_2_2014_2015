f892ba743b1042c44c29e3abdcf34aa4864da883
Lukáš Drahník <L.Drahnik@gmail.com>
2014-12-17 21:41:41 +0100

    Fixed errors

--- inp/inp2014-proj2/fpga/cpu.vhd
+++ inp/inp2014-proj2/fpga/cpu.vhd
@@ -47,6 +47,7 @@ architecture behavioral of cpu is
 signal pc_reg        : std_logic_vector(12 downto 0);
 signal pc_ld         : std_logic;                 -- signal, ktery v '1' rika, ze to chci ulozit
 signal pc_inc        : std_logic;
+signal pc_dec        : std_logic;
 
 -- Instruction register IREG
 signal ireg_reg      : std_logic_vector(7 downto 0);
@@ -59,11 +60,16 @@ signal data_dec      : std_logic;
 signal data_ld       : std_logic;
 
 -- Instruction decoder
-type inst_type is (dec_ptr, inc_ptr, inc_value, dev_value, while_start, while_end, print_value, read_value, halt);
+type inst_type is (dec_ptr, inc_ptr, inc_value, dec_value, while_start, while_end, print_value, read_value, nothing, skip);
 signal ireg_decode   : inst_type;
 
 -- Data or Program
-signal acces_to_data : boolean := true;
+signal access_to_data : boolean := true;
+
+-- Final state machine states
+type fsm_state is (s_00, s_0, s_1, s_2, s_3, s_dec_ptr, s_inc_ptr, s_inc_value, s_inc_value_2, s_dec_value, s_dec_value_2, s_while_start, s_while_end, s_print_value, s_print_value_2, s_read_value, s_nothing, s_skip);
+signal present_state    : fsm_state;
+signal next_state    : fsm_state;
 
 begin
 
@@ -71,9 +77,9 @@ begin
 -- Program counter PC
 pc_cntr: process (RESET, CLK)
 begin
-   if (RESET='1') then
-      pc_reg <= (others=>'0');
-   elseif (CLK'event) and (CLK='1') then
+   if (RESET = '1') then
+      pc_reg <= (others => '0');
+   elsif (CLK'event) and (CLK = '1') then
       if (pc_dec = '1') and (pc_inc = '0') then
          pc_reg <= pc_reg - 1;
       end if;
@@ -81,33 +87,33 @@ begin
             pc_reg <= pc_reg + 1;
       end if;
    end if;
-end process
+end process;
 
 
 -- Instruction register IREG
 ireg: process (RESET, CLK)
 begin
-   if (RESET='1') then
-      ireg_reg <= (others=>'0');
-   elseif (CLK'event) and (CLK='1') then
-      if (ireg_ld='1') then
+   if (RESET = '1') then
+      ireg_reg <= (others => '0');
+   elsif (CLK'event) and (CLK = '1') then
+      if (ireg_ld = '1') then
          ireg_reg <= DATA_RDATA;
       end if;
    end if;
-end process
+end process;
 
 
 -- Data pointer
 data_ptr: process(CLK, RESET)
    begin
-      if (RESET='1') then
+      if (RESET = '1') then
          data_reg <= "1000000000000";
       elsif (RESET = '0') and (CLK'event) and (CLK = '1') then
-      if (PTR_decrement = '1') and (PTR_increment = '0') then           -- decrement
+      if (data_dec = '1') and (data_inc = '0') then           -- decrement
          data_reg <= data_reg - 1;
       end if;
 
-      if (PTR_decrement = '0') and (PTR_increment = '1') then           -- increment
+      if (data_dec = '0') and (data_inc = '1') then           -- increment
          data_reg <= data_reg + 1;
       end if;
    end if;
@@ -116,8 +122,8 @@ end process;
 
 
 -- Switch between two memories (Von Neumann)
-access: process()
-   begin
+process (CLK)
+begin
    if (not(access_to_data)) then
       DATA_ADDR <= pc_reg;
    else
@@ -126,9 +132,9 @@ access: process()
 end process;
 
 -- Instruction decoder -> decode ASCII value to instruction
-process (ireg)
+process (ireg_reg)
 begin
-   case ireg is
+   case ireg_reg is
       when X"3C" => ireg_decode <= dec_ptr;              --    "<"
       when X"3E" => ireg_decode <= inc_ptr;              --    ">"
       when X"2B" => ireg_decode <= inc_value;            --    "+"
@@ -137,28 +143,29 @@ begin
       when X"5D" => ireg_decode <= while_end;            --    "]"
       when X"2E" => ireg_decode <= print_value;          --    "."
       when X"2C" => ireg_decode <= read_value;           --    ","
-      when X"00" => ireg_decode <= null;                 --    "null"
+      when X"00" => ireg_decode <= nothing;              --    "nothing"
       when others => ireg_decode <= skip;                --    skip others
    end case;
 end process;
 
 
 -- Final state machine
-fsm: process(present_state, ireg_decode, OUT_DATA)
+process (present_state, ireg_decode, OUT_BUSY)
 begin
-      next_state <= s00;
+      next_state <= s_00;
 
       DATA_EN  <= '0';
-      dec_ptr  <= '0';
-      inc_ptr  <= '0';
       ireg_ld  <= '0';
-      pc_reg   <= '0';
+
+      -- pc
       pc_ld    <= '0';
       pc_inc   <= '0';
+      pc_dec   <= '0';
+
+      -- data
       data_ld  <= '0';
 
       case present_state is
-
          when s_00 =>
             next_state <= s_0;
 
@@ -176,8 +183,8 @@ begin
           -- Decode instruction
          when s_2 =>
             case ireg_decode is
-               when null =>
-                  next_state <= s_null;
+               when nothing =>
+                  next_state <= s_nothing;
 
                when inc_ptr =>
                   next_state <= s_inc_ptr;
@@ -185,10 +192,10 @@ begin
                when dec_ptr =>
                   next_state <= s_dec_ptr;
 
-               when inc_val =>
+               when inc_value =>
                   next_state <= s_inc_value;
 
-               when dec_val =>
+               when dec_value =>
                   next_state <= s_dec_value;
 
                when while_start =>
@@ -208,9 +215,9 @@ begin
             end case;
 
 
-         -- null
-         when s_null =>
-            next_state <= s_null;
+         -- nothing
+         when s_nothing =>
+            next_state <= s_nothing;
 
          -- skip
          when s_skip =>
@@ -259,20 +266,20 @@ begin
 
          -- "-"      ->  DATA_RDATA <- ram[PTR]
          --          ->  ram[PTR] <- DATA_RDATA - 1; PC <- PC + 1
-         when s_dec_val =>
+         when s_dec_value =>
             DATA_EN <= '1';
             data_ld <= '1';
             DATA_RDWR <= '0';
             next_state <= s_dec_value_2;
 
-         when s_dec_val_2 =>
+         when s_dec_value_2 =>
             data_ld <= '1';
             DATA_EN <= '1';
             DATA_RDWR <= '1';
             DATA_WDATA <= DATA_RDATA - 1;
 
-            PC_decrement <= '0';
-            PC_increment <= '1';
+            pc_dec <= '0';
+            pc_inc <= '1';
             next_state <= s_0;
 
          -- "."      ->  OUT_DATA <- ram[PTR]
@@ -288,8 +295,8 @@ begin
             if (OUT_BUSY = '0') then
                OUT_WE <= '1';
                OUT_DATA <= DATA_RDATA;
-               PC_decrement <= '0';
-               PC_increment <= '1';
+               pc_dec <= '0';
+               pc_inc <= '1';
                next_state <= s_0;
             else
                next_state <= s_print_value_2;
